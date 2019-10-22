824e86cd1fdc1a56918ce6f449d75f06519e188b
Alexey Kuleshevich <alexey@kuleshevi.ch>
2019-10-23 01:24:28 +0300

    A bit of formatting and experimenting with alternatives

--- repa-examples/examples/Canny/src-massiv/Main.hs
+++ repa-examples/examples/Canny/src-massiv/Main.hs
@@ -37,10 +37,9 @@ orientVert      = 128   :: Word8
 orientNegDiag   = 192   :: Word8
 orientHoriz     = 255   :: Word8
 
-data Edge       = None | Weak | Strong
-edge None   = 0     :: Pixel Y Word8
-edge Weak   = 128   :: Pixel Y Word8
-edge Strong = 255   :: Pixel Y Word8
+edgeNone   = 0     :: Pixel Y Word8
+edgeWeak   = 128   :: Pixel Y Word8
+edgeStrong = 255   :: Pixel Y Word8
 
 
 -- Main routine ---------------------------------------------------------------
@@ -72,8 +71,8 @@ run loops threshLow threshHigh fileIn fileOut
         writeImage fileOut arrResult
 
 process :: Int -> Float -> Float -> Image S RGB Word8 -> IO (Image S Y Word8)
-process loops threshLow threshHigh arrInput
- = do   arrGrey         <- timeStage loops "toGreyScale"
+process loops threshLow threshHigh arrInput = do
+        arrGrey         <- timeStage loops "toGreyScale"
                            (toGreyScale arrInput)
 
         arrBluredX      <- timeStage loops "blurX"
@@ -112,8 +111,7 @@ timeStage
         -> IO a
         -> IO a
 
-timeStage loops name fn
- = do
+timeStage loops name fn = do
         let burn !n
              = do !arr  <- fn
                   if n <= 1 then return arr
@@ -142,10 +140,10 @@ toGreyScale = pure . compute . A.map ((*255) . toPixelYF)
 {-# NOINLINE toGreyScale #-}
 
 toPixelYF :: Pixel RGB Word8 -> Pixel Y Float
-toPixelYF (PixelRGB r g b)
- = let  r'      = fromIntegral (fromIntegral r :: Int) / 255
-        g'      = fromIntegral (fromIntegral g :: Int) / 255
-        b'      = fromIntegral (fromIntegral b :: Int) / 255
+toPixelYF (PixelRGB r g b) =
+  let !r' = fromIntegral r / 255
+      !g' = fromIntegral g / 255
+      !b' = fromIntegral b / 255
    in PixelY (r' * 0.3 + g' * 0.59 + b' * 0.11)
 {-# INLINE toPixelYF #-}
 
@@ -163,6 +161,26 @@ blurSepX =
        get (0 :.  2)
     )
 {-# NOINLINE blurSepX #-}
+
+-- gaussianX :: Num e => Stencil Ix2 e e
+-- gaussianX =
+--   makeCorrelationStencil (Sz2 1 5) (0 :. 2) $ \get ->
+--     get (0 :. -2) 1 .
+--     get (0 :. -1) 4 .
+--     get (0 :.  0) 6 .
+--     get (0 :.  1) 4 .
+--     get (0 :.  2) 1
+-- {-# INLINE gaussianX #-}
+
+-- gaussianY :: Num e => Stencil Ix2 e e
+-- gaussianY =
+--   makeCorrelationStencil (Sz2 5 1) (2 :. 0) $ \get ->
+--     get (-2 :. 0) 1 .
+--     get (-1 :. 0) 4 .
+--     get ( 0 :. 0) 6 .
+--     get ( 1 :. 0) 4 .
+--     get ( 2 :. 0) 1
+-- {-# INLINE gaussianY #-}
 
 
 -- | Separable Gaussian blur in the Y direction.
@@ -226,47 +244,43 @@ gradientY = pure . compute . mapStencil Edge sobelY
 -- sobelX :: (Default e, Num e) => Stencil Ix2 e e
 -- sobelX =
 --   makeStencil (Sz 3) (1 :. 1) $
---     \ f -> f (-1 :.  1)     +
---            f ( 0 :.  1) * 2 +
---            f ( 1 :.  1)     -
---            f (-1 :. -1)     -
---            f ( 0 :. -1) * 2 -
---            f ( 1 :. -1)
+--   \ f -> f (-1 :. -1) * (-1) +
+--          f ( 0 :. -1) * (-2) +
+--          f ( 1 :. -1) * (-1) +
+--          f (-1 :.  1)        +
+--          f ( 0 :.  1) *    2 +
+--          f ( 1 :.  1)
 -- {-# INLINE sobelX #-}
 
 
 -- sobelY :: (Default e, Num e) => Stencil Ix2 e e
 -- sobelY =
 --   makeStencil (Sz 3) (1 :. 1) $
---     \ f -> f ( 1 :. -1)     +
---            f ( 1 :.  0) * 2 +
---            f ( 1 :.  1)     -
---            f (-1 :. -1)     -
---            f (-1 :.  0) * 2 -
---            f (-1 :.  1)
+--   \ f -> f (-1 :. -1) * (-1) +
+--          f (-1 :.  0) * (-2) +
+--          f (-1 :.  1) * (-1) +
+--          f ( 1 :. -1)        +
+--          f ( 1 :.  0) *    2 +
+--          f ( 1 :.  1)
 -- {-# INLINE sobelY #-}
 
 sobelX :: Num e => Stencil Ix2 e e
-sobelX =
-  makeCorrelationStencil (Sz 3) (1 :. 1) $
-  \ f -> f (-1 :. -1) (-1) .
-         f ( 0 :. -1) (-2) .
-         f ( 1 :. -1) (-1) .
-         f (-1 :.  1)   1  .
-         f ( 0 :.  1)   2  .
-         f ( 1 :.  1)   1
+sobelX = makeCorrelationStencil (Sz 3) (1 :. 1) $ \ f -> f (-1 :. -1) (-1) .
+                                                         f ( 0 :. -1) (-2) .
+                                                         f ( 1 :. -1) (-1) .
+                                                         f (-1 :.  1)   1  .
+                                                         f ( 0 :.  1)   2  .
+                                                         f ( 1 :.  1)   1
 {-# INLINE sobelX #-}
 
 
 sobelY :: Num e => Stencil Ix2 e e
-sobelY =
-  makeCorrelationStencil (Sz 3) (1 :. 1) $
-  \ f -> f (-1 :. -1) (-1) .
-         f (-1 :.  0) (-2) .
-         f (-1 :.  1) (-1) .
-         f ( 1 :. -1)   1  .
-         f ( 1 :.  0)   2  .
-         f ( 1 :.  1)   1
+sobelY = makeCorrelationStencil (Sz 3) (1 :. 1) $ \ f -> f (-1 :. -1) (-1) .
+                                                         f (-1 :.  0) (-2) .
+                                                         f (-1 :.  1) (-1) .
+                                                         f ( 1 :. -1)   1  .
+                                                         f ( 1 :.  0)   2  .
+                                                         f ( 1 :.  1)   1
 {-# INLINE sobelY #-}
 
 
@@ -332,14 +346,14 @@ suppress !threshLow !threshHigh (!dMag, !dOrient)
   $ mapStencil (Fill 0) (makeUnsafeStencil 3 1 comparePts) dMag
  where  {-# INLINE comparePts #-}
         comparePts !ix getMag
-         | o == orientUndef     = edge None
+         | o == orientUndef     = edgeNone
          | o == orientHoriz     = isMax (getMag ( 0 :. -1)) (getMag ( 0 :.  1))
          | o == orientVert      = isMax (getMag (-1 :.  0)) (getMag ( 1 :.  0))
          | o == orientNegDiag   = isMax (getMag (-1 :. 1)) (getMag ( 1 :.  -1))
          | o == orientPosDiag   = isMax (getMag (-1 :. -1)) (getMag ( 1 :.  1))
          -- | o == orientNegDiag   = isMax (getMag (-1 :. -1)) (getMag ( 1 :.  1)) --?????
          -- | o == orientPosDiag   = isMax (getMag (-1 :.  1)) (getMag ( 1 :. -1)) --?????
-         | otherwise            = edge None
+         | otherwise            = edgeNone
 
          where
           !o = unsafeIndex dOrient ix
@@ -347,23 +361,23 @@ suppress !threshLow !threshHigh (!dMag, !dOrient)
 
           {-# INLINE isMax #-}
           isMax !intensity1 !intensity2
-            | m < threshLow     = edge None
-            | m < intensity1    = edge None
-            | m < intensity2    = edge None
-            | m < threshHigh    = edge Weak
-            | otherwise         = edge Strong
+            | m < threshLow     = edgeNone
+            | m < intensity1    = edgeNone
+            | m < intensity2    = edgeNone
+            | m < threshHigh    = edgeWeak
+            | otherwise         = edgeStrong
 {-# NOINLINE suppress #-}
 
 
 -- | Select indices of strong edges.
-selectStrong :: Image S Y Word8 -> IO (Array U Ix1 Ix2)
+selectStrong :: Image S Y Word8 -> IO (Array S Ix1 Ix1)
 selectStrong =
   pure . compute .
   imapMaybeS
     (\ !ix !e ->
-       if e == edge Strong
+       if e == edgeStrong
          then Just ix
-         else Nothing)
+         else Nothing) . flatten
 {-# NOINLINE selectStrong #-}
 
 
@@ -371,50 +385,51 @@ selectStrong =
 --   Also trace out weak edges that are connected to strong edges.
 wildfire ::
      Image S Y Word8 -- ^ Image with strong and weak edges set.
-  -> Array U Ix1 Ix2 -- ^ Array containing indices of strong edges.
+  -> Array S Ix1 Ix1 -- ^ Array containing indices of strong edges.
   -> IO (Image S Y Word8)
 wildfire img vStrong = do
   vStrong' <- A.thaw vStrong
   -- Stack of image indices we still need to consider.
   vStack <- A.unsafeLinearGrow vStrong' (Sz lenImg)
   -- Burn in new edges.
-  vImg <- A.new (size img)
+  vImg <- A.new sz
   burn vImg vStack (unSz (size vStrong))
   unsafeFreeze (getComp img) vImg
   where
-    lenImg = totalElem (A.size img)
+    !sz = A.size img
+    !lenImg = totalElem sz
     burn ::
          MArray RealWorld S Ix2 (Pixel Y Word8)
-      -> MArray RealWorld U Ix1 Ix2
+      -> MArray RealWorld S Ix1 Ix1
       -> Int
       -> IO ()
-    burn !vImg !vStack !top
-      | top == 0 = return ()
-      | otherwise = do
-        let !top' = top - 1
-        ix@(y :. x) <- readM vStack top'
-        let push t = pushWeak vImg vStack t
-            {-# INLINE push #-}
-        unsafeWrite vImg ix (edge Strong) >> push (y - 1 :. x - 1) top' >>=
-          push (y - 1 :. x) >>=
-          push (y - 1 :. x + 1) >>=
-          push (y :. x - 1) >>=
-          push (y :. x + 1) >>=
-          push (y + 1 :. x - 1) >>=
-          push (y + 1 :. x) >>=
-          push (y + 1 :. x + 1) >>=
-          burn vImg vStack
-    -- If this ix is weak in the source then set it to strong in the
-    -- result and push the ix onto the stack.
-    pushWeak vImg vStack ix top = do
-      case indexM img ix of
-        Nothing -> pure top
-        Just xSrc -> do
-          xDst <- unsafeRead vImg ix
-          if xDst == edge None && xSrc == edge Weak
-            then do
-              unsafeWrite vStack top ix
-              return (top + 1)
-            else return top
-    {-# INLINE pushWeak #-}
+    burn !vImg !vStack = go
+      where
+        push !ix !t =
+          case indexM img ix of
+            Nothing -> pure t
+            Just xSrc -> do
+              xDst <- unsafeRead vImg ix
+              if xDst == edgeNone && xSrc == edgeWeak
+                -- If this ix is weak in the source then set it to strong in the result
+                -- and push the ix onto the stack.
+                then do
+                  unsafeWrite vStack t $ toLinearIndex sz ix
+                  return (t + 1)
+                else return t
+        {-# INLINE push #-}
+        go !top
+          | top == 0 = return ()
+          | otherwise = do
+            let !top' = top - 1
+            ix@(y :. x) <- fromLinearIndex sz <$> readM vStack top'
+            unsafeWrite vImg ix (edgeStrong)
+            push (y - 1 :. x - 1) top' >>= push (y - 1 :. x) >>=
+              push (y - 1 :. x + 1) >>=
+              push (y :. x - 1) >>=
+              push (y :. x + 1) >>=
+              push (y + 1 :. x - 1) >>=
+              push (y + 1 :. x) >>=
+              push (y + 1 :. x + 1) >>=
+              go
 {-# NOINLINE wildfire #-}
